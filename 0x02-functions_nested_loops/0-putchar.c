e7fa479eafeb670053264333c0c9ed492425f4d1
Joseph Salaon <salaonjoseph@gmail.com>
2023-02-22 02:23:02 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,6 @@
-/*
- * File: _putchar.c
- * 
- *
- */
+#include <stdio.h>
 
-#include "stdio.h"
-
-/**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+int main() {
+    printf("_putchar\n");
+    return 0;
 }
