4eba3d1c77c42ac8c1dcf1dcb32d7c156e6e41de
livingthdrm <salaonjoseph@gmail.com>
2023-03-06 14:13:32 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - Prints line as shown below
+ * main - prints line of text without using printf or puts
  *
- * Return: Always 1.
-*/
+ * Return: Always 1
+ */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		       	59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, 59);
 	return (1);
 }
