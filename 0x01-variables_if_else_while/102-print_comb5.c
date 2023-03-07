b5453e3d94f4cf7ff6afa8517ed6c533f1e20b4b
livingthdrm <salaonjoseph@gmail.com>
2023-03-07 12:50:34 -0800

    j

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar(j / 10 + '0');
 				putchar(j % 10 + '0');
-				if (i != 99 || k != 99)
+				if (i != 99 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
