7c5c569e65e3677ac508962fb06373f134bac637
livingthdrm <salaonjoseph@gmail.com>
2023-03-07 15:15:33 -0800

    fixes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,11 +17,11 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 			{
-				printf("%d", i);
+				printf("%d,", i);
 			}
 			else if (i == 98)
 			{
-				printf("%d\n", i);
+				printf("%d,\n", i);
 			}
 		else if (n >= 98)
 		{
@@ -29,11 +29,11 @@ void print_to_98(int n)
 			{
 				if (j != 98)
 				{
-					printf("%d", j);
+					printf("%d,", j);
 				}
 				else if (j == 98)
 				{
-					printf("%d\n", j);
+					printf("%d,\n", j);
 				}
 			}
 		}
