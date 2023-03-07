e491cc9921e3779dc869d609a2e27daf45a9e561
livingthdrm <salaonjoseph@gmail.com>
2023-03-07 13:26:28 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 
-int main() {
-    printf("_putchar\n");
-    return 0;
+/**
+ * main - entry point
+ *
+ * description - A program that prints _putchar
+ *
+ * Return: On success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
