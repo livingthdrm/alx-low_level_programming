49ce40433865411458fccbcd117c97cc23cf72e7
livingthdrm <salaonjoseph@gmail.com>
2023-03-24 16:53:35 -0700

    fix 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 99; i++)
 	{
 		for (j = i; j < 100; j++)
 		{
@@ -23,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar(j / 10 + '0');
 				putchar(j % 10 + '0');
-				if (i != 99 || j != 99)
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
