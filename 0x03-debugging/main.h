9c75716fac09693b39f950a141b249129f488320
livingthdrm <salaonjoseph@gmail.com>
2023-06-15 18:53:01 -0700

    Finding the largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,36 @@
 
 void positive_or_negative(int i);
 
+/**
+ * largest number - returns the largest of three numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if(a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		printf("The numbers are equal");
+	}
+
+	return (largest);
+}
+
 #endif /* MAIN_H */
