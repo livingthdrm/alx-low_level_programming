518bea4b723c84ed43b80f90beb2cb07526adbf3
livingthdrm <salaonjoseph@gmail.com>
2023-07-04 11:19:57 -0700

    push bugs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,11 @@
  * @argv: array of arguments
  * Return: always 0
  */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
+	(void)argv;
+
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
