c5644f27a8d1536d4ea35f0c62a5ba2b65df8d01
livingthdrm <salaonjoseph@gmail.com>
2023-07-04 10:58:04 -0700

    bugs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
 
 /**
  * main -  A program that prints its name followed by a new line
- *
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	char *pname = strrchr(argv[0], '/');
 
