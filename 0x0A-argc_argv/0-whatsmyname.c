f8c5b833f191ae8657ae14b519cc00c4f1b3c3d9
livingthdrm <salaonjoseph@gmail.com>
2023-07-04 11:07:53 -0700

    bugs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,19 +9,10 @@
  * @argv: array of arguments
  * Return: Always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	char *pname = strrchr(argv[0], '/');
 
-	if (pname == NULL)
-	{
-		pname = argv[0];
-	}
-	else
-	{
-		pname++;
-	}
-	printf("%s\n", pname);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
