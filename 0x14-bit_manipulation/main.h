757662982074f001a075ac7afa9955d161c6bfc3
livingthdrm <salaonjoseph@gmail.com>
2023-08-26 12:47:28 -0700

    m

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
 
-=======
->>>>>>> 720bb81d230372293a482c4b6cac6aa4fd85b018
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
