018d6a8c12f85838e6f788c709b87a5a213dc92e
livingthdrm <salaonjoseph@gmail.com>
2023-10-02 07:05:14 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts - prints a string
+ *
+ * @str: string to be printe
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	return (str);
+}
