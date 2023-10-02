5a1ac689b819551034e405b2843973a956239216
livingthdrm <salaonjoseph@gmail.com>
2023-10-02 04:45:37 -0700

    point

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * int _strlen - function that checks teh length of a string
+ *
+ * @char *s - string to be evaluated
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int count = 0, i = 0;
+	while(*s !== "\0")
+	{
+		count += *s[i];
+		count++;
+	}
+	return count;
+}
