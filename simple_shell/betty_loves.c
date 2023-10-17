974d9afa1fde7cae835c4a69dd3fe3bfd05060a0
livingthdrm <salaonjoseph@gmail.com>
2023-10-17 09:11:59 -0700

    all

---
+++ simple_shell/betty_loves.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - A program that swaps two variables using pointers
+ *
+ * Return - void
+ */
+void main()
+{
+	int* a, b;
+	int temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
