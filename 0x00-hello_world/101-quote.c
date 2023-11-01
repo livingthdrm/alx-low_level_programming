90024214bfd4b4bd384f210bb9f37b15464a0b66
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 13:50:54 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,5 @@ int main(void)
 	char *string[100];
 
 	*string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	return (write(2, *string, 1));
+	return (write(2, *string, 59));
 }
