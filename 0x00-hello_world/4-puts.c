4eb246038e006d6cdf35fb9737553ada2ddde9b5
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 05:33:15 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,10 @@
 /**
  * main - c program that prints a sentence
  * only using puts function
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
 	char *c = "Programming is like building a multilingual puzzle\n";
