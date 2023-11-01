d4493232604ce847b6cfe9f21854cb4d6db1b848
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 13:41:33 -0700

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,13 @@
 /**
  * main - prints a string to stderr
  *
+ * Return - to be  1
  */
-void main(void)
+int main(void)
 {
 	char *string[100];
 
 	*string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, &string, 1);
+	return (1);
 }
