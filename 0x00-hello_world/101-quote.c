0eee5fa96ed8ea84d113a44a9def36e91e7d29b6
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 13:43:42 -0700

    fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,5 @@ int main(void)
 	char *string[100];
 
 	*string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, &string, 1);
-	return (1);
+	return (write(2, &string, 1));
 }
