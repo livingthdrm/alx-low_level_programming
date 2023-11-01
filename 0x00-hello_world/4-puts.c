ae6f8c532cbd19869932c2a9e68edbebe9c7a37d
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 10:35:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char str[100];
 
-	str[] = "\"Programming is like building a multilingual puzzle";
+	str = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
 }
