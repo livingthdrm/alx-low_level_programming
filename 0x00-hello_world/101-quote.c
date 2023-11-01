76136d369fc980dd5448623ab6a1588788776b80
livingthdrm <salaonjoseph@gmail.com>
2023-11-01 13:46:48 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - prints a string to stderr
  *
- * Return - to be  1
+ * Return on success  1
  */
 int main(void)
 {
 	char *string[100];
 
 	*string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	return (write(2, &string, 1));
+	return (write(2, *string, 1));
 }
