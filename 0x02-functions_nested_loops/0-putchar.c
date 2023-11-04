a64cb586389ab312892fb080bb4112c1a83ea3a0
livingthdrm <salaonjoseph@gmail.com>
2023-11-04 14:29:38 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that print out _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c = "_putchar";
+
+	_putchar('0' + c);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,9 @@
+
+0. _putchar
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+
+Write a program that prints _putchar, followed by a new line.
+
+    The program should return 0
+
