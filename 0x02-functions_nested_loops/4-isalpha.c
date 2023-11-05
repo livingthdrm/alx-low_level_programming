231de2bcc09b2e24da0dad8e308e942472f277c7
livingthdrm <salaonjoseph@gmail.com>
2023-11-04 17:09:47 -0700

    is letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether a character is alphanumerical
+ * @c: character to check
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <== 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
