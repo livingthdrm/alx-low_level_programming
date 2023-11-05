3066e0387db59b97d65b1f147e6878491d2efc2e
livingthdrm <salaonjoseph@gmail.com>
2023-11-04 17:39:05 -0700

    fix sign 2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+1');
+		return ("+1");
 	}
 	else if (n == 0)
 	{
-		return ('00');
+		return ("00");
 	}
 	else if (n < 0)
 	{
-		return ('-1');
+		return ("-1");
 	}
 	return (0);
 }
