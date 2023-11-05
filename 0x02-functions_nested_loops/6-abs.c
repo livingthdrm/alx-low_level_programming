107cec697474682321517e9257fc3f0a8b355029
livingthdrm <salaonjoseph@gmail.com>
2023-11-04 18:18:24 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of an integer
+ * @int: value to quantify
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	if (int > 0)
+	{
+		return (int);
+	}
+	else if (int == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-int);
+	}
+	return (0);
+}
