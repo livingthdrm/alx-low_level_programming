9aa5ce8e116af8cd0b29f56633a3cf98ffc6c441
livingthdrm <salaonjoseph@gmail.com>
2023-11-07 13:53:08 -0800

    jack please

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,2 +1,19 @@
 #include "main.h"
+/**
+ * jack_bauer - a program that prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i = 0, j =  0;
 
+	while (i < 24)
+	{
+		while (j < 60)
+		{
+			_putchar('0' + i + 58 + j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
