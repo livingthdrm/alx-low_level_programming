6cf48bca7b77534dd78b0fb43d91026d1b9aedae
livingthdrm <salaonjoseph@gmail.com>
2023-11-07 17:13:25 -0800

    fix str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - calculates the length of a string
+ * @s: string to evaluate
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count = *s;
+		s++;
+	}
+	_putchar('0' + count);
+}
