f150f94e69be66ffe6d119bdbd1b93670316ef32
livingthdrm <salaonjoseph@gmail.com>
2023-11-08 15:16:30 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ *
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	*s = ('\0' - 1);
+
+	while (*s)
+	{
+		return (*s);
+		s--;
+	}
+	_putchar('\n');
