92fa53e34e333695fb30e3c537b7d43a3430209d
livingthdrm <salaonjoseph@gmail.com>
2023-11-08 15:05:20 -0800

    print str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * @str: the string to write out
+ */
+void _puts(char *str)
+{
+	while (*str != '0')
+	{
+		_putchar(*str);
+		s++;
+	}
+	_putchar('\n');
+}
