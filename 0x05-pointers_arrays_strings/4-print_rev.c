3de6f7b99effaeb7455366e7bcf964a3d0278b81
livingthdrm <salaonjoseph@gmail.com>
2023-11-11 15:48:44 -0800

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,19 +9,16 @@ void print_rev(char *s)
 {
 	int count = 0;
 
-	while (*s != '\0')
+	while (*s < '\0')
 	{
 		s++;
 		count++;
 	}
 	while (count >= 0)
 	{
-		if (*s == '\0')
-		{
-			_putchar(*s);
-			s--;
-			count--;
-		}
+		_putchar(*s);
+		s--;
+		count--;
 	}
 	_putchar('\n');
 }
