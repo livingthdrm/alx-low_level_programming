dcc383d3274cddd09e4e7341a49566ac00ece1c9
livingthdrm <salaonjoseph@gmail.com>
2023-11-11 19:38:33 -0800

    fix 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ */
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
