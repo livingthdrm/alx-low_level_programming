a795c012dc557ded72854cffc4a6e30d0fbd9780
livingthdrm <salaonjoseph@gmail.com>
2023-11-15 00:52:35 -0800

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,24 @@
 int main(void)
 {
 	int i = 1;
-
+	
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i % 3 != 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
-			printf("%d ", i);
+			printf("Fizz ");
 		}
 		i++;
 	}
