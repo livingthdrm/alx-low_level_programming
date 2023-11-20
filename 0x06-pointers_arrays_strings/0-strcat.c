17b737ac5e9288bd4cf2c7eeb4669d6fbef57d61
livingthdrm <salaonjoseph@gmail.com>
2023-11-20 12:00:54 -0800

    first attempt at concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcat - A function that concatenantes 2 strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *resulting;
+
+	while (*dest != '\0')
+	{
+		*resulting += *dest;
+		dest++;
+		resulting++;
+	}
+	while (*src != '\0')
+	{
+		*resulting += src;
+		src++;
+		resulting++;
+	}
+	return (*resulting);
+	_putchar('\0');
+}
