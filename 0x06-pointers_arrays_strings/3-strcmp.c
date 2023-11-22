fd2a97bfdfbbed1628715142895426b8971035e5
livingthdrm <salaonjoseph@gmail.com>
2023-11-22 09:28:15 -0800

    fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
@@ -16,11 +16,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-		else if (*s1 < *s2)
-		{
-			return (*s1 - *s2);
-		}
-		else if (*s1 > *s2)
+		else
 		{
 			return (*s1 - *s2);
 		}
