f7b17d9c76ad6773a86d308ec0f3bdb2958fe490
livingthdrm <salaonjoseph@gmail.com>
2023-11-22 08:54:18 -0800

    3-string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0')
+	{
+		while (*s2 != '\0')
+		{
+			if (*s1 == *s2)
+			{
+				return (0);
+			}
+			else if (*s1 < *s2)
+			{
+				return (-15);
+			}
+			else if (*s1 > *s2)
+			{
+				return (15);
+			}
+			s1++;
+			s2++;
+		}
+	}
+	return (0);
+}
