09c9b269a10a293652572597f06d2a8511cba683
livingthdrm <salaonjoseph@gmail.com>
2023-11-22 09:23:08 -0800

    fix 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,11 +15,11 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (*s1 < *s2)
 		{
-			return (-1);
+			return (*s1 - *s2);
 		}
 		else if (*s1 > *s2)
 		{
-			return (1);
+			return (*s1 - *s2);
 		}
 		s1++;
 		s2++;
