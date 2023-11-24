1fbab599df1dc86867c1010c0d56685ba877526a
livingthdrm <salaonjoseph@gmail.com>
2023-11-24 03:01:38 -0800

    fix 4 reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int *temp = a;
+	int temp;
+	int i;
 
-	for (int i = 0; i < n/2; i++)
+	for (i = 0; i < n/2; i++)
 	{
-		int temp = *a;
+		temp = a[i];
 		a[i] = a[n - 1 - i];
 		a[n - 1 - i] = temp;
-
+	}
 }
