adf562d2ee27f6e18cf059b3ace40a0f608c1505
livingthdrm <salaonjoseph@gmail.com>
2023-11-26 12:35:57 -0800

    fix temp

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *string_touppe - function that changes all lowercase
+ * letters of a string to uppercase
+ * @s: The string to evaluate
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	char i = 'a', j = 'A';
+	int k = 1;
+
+	while (*str != '\0')
+	return (0);
+}	
