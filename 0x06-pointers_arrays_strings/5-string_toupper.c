f097f682e02834c17022d443a86b0de21e74f4f4
livingthdrm <salaonjoseph@gmail.com>
2023-11-26 13:06:53 -0800

    fix string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * *string_touppe - function that changes all lowercase
+ * string_toupper - function that changes all lowercase
  * letters of a string to uppercase
- * @s: The string to evaluate
+ * @str: The string to evaluate
  * Return: 0
  */
 char *string_toupper(char *str)
 {
 	char *temp = str;
-	
+
 	while (*temp != '\0')
 	{
 		if (*temp >= 'a' && *temp <= 'z')
