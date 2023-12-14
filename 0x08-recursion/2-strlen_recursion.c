b2dffaf1f18c3e0c34c4da13887d0292b6c9a48c
livingthdrm <salaonjoseph@gmail.com>
2023-12-14 15:19:11 -0800

    fix length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (*s);
+		return (0);
 	}
-	*s += _strlen_recursion(s + 1);
-	return (*s);
+	return (1 + _strlen_recursion(s + 1));
 }
