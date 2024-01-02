f102e74f96fd462225b7982c64c840164e154d60
livingthdrm <salaonjoseph@gmail.com>
2024-01-02 06:28:48 -0800

    commit task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - a program that print the number of
+ * variables it has
+ * @argc: numbe rof variables
+ * @argv: variables
+ * Return: 0;
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
