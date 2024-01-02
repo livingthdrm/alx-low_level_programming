7120bd3d5e0ae2345f6ce621caf67696b02f81e7
livingthdrm <salaonjoseph@gmail.com>
2024-01-02 07:47:50 -0800

    fix fourth task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,13 +4,21 @@
  * main - a program that multiples two integers
  * @argc: number of parameters (3)
  * @argv: parameters
+ * Return: always 0
  */
 int main(int argc, char **argv)
 {
 	int result;
 
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	else
+	{
 	result = atoi(argv[1]) * atoi(argv[2]);
 	(void)argc;
 	printf("%d\n", result);
+	}
 	return (0);
 }
