45d8efde66b1186ab8d2406a329d99d9d9e9243a
root <salaonjoseph@gmail.com>
2025-04-09 13:53:01 -0700

    a program that checks whether a randomly generated number is positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - random number generated and checked whether +, - or 0
+ *
+ * Return: always 0
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-		printf("%d is positive", n );
+	if (n > 0)
+	{
+		printf("%d is positive", n);
 	}
 	else if (n < 0)
 	{
 		printf("%d is negative", n);
 	}
-	else {
+	else
+	{
 		printf("%d is zero", n);
 	}
 	return (0);
