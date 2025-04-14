f30a45db0985d2538263e13ee4955e57d097de76
root <salaonjoseph@gmail.com>
2025-04-14 06:20:35 -0700

    a program that prints all base ten digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all single digit numbersof base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 48;
+
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
