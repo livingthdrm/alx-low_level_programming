6911aaad0f22b3f0d39dd096538110c9ad9f1f0c
root <salaonjoseph@gmail.com>
2025-04-16 19:34:04 -0700

    a program that prints all possible different combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible different combinations of 2 digits
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		int j = i + 1;
+
+		while (j <= 57)
+		{
+			putchar(i);
+			putchar(j);
+
+			if ((i != 56 || j != 57))
+			{	
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
