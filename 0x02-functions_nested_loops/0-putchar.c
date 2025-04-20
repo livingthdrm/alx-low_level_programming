7c15651b1aaf2c5b8d9f0c97709636e1d5ff3de5
root <salaonjoseph@gmail.com>
2025-04-19 19:26:28 -0700

    a program that prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
- * main - a program that print out _putchar
+ * main - a program that prints _putchar
  *
- * Return: 0
+ * Return: always 0
  */
 int main(void)
 {
-	char *c = "_putchar";
-
-	while (*c != '\0')
-	{
-		_putchar(*c);
-		c++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
