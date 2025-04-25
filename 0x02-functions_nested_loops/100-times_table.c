bd18e8ea1bf00ff99659eb43e8238c8548292151
root <salaonjoseph@gmail.com>
2025-04-25 02:38:43 -0700

    a function to print the times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,20 +8,19 @@
  */
 void print_times_table(int n)
 {
-	int i = 0, j, result;
+	int i, j, result;
+
 	if (n >= 0 && n <= 15)
 	{
-	while (i <= n)
+	for (i = 0; i <= n; i++)
 	{
-		j = 0;
-
-		while (j <= n)
+		for (j = 0; j <= n, j++)
 		{
 			result = i * j;
 
 			if (j == 0)
 			{
-				_putchar('0' + result);
+				_putchar('0');
 			}
 			else
 			{
@@ -46,10 +45,8 @@ void print_times_table(int n)
 				_putchar('0' + result % 10);
 			}
 			}
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 	}
 }
