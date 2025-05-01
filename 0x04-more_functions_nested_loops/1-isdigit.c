85db5031ce6637b8e0f388ddb9835cf29418a950
root <salaonjoseph@gmail.com>
2025-05-01 09:46:35 -0700

    a program to check for numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isdigit - a program to check for digit 0 - 9
+ * @c: the digit to check
+ *
+ * Return: 1 if digit 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
