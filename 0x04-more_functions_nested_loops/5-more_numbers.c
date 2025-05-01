0d41ba751b8208f2f7a537f0b85ae63b8dfffbcc
root <salaonjoseph@gmail.com>
2025-05-01 11:00:17 -0700

    a program to print numbers 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 
 		while (j <= 14)
 		{
-			if (j < 10)
-			{
-				_putchar('0' + j);
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
 			}
+				_putchar('0' + j % 10);
 			j++;
 		}
 		i++;
