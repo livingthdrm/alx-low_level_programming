a297ace3817edeb39ae019927b438e9d0834dd2b
root <salaonjoseph@gmail.com>
2025-05-01 10:43:55 -0700

    a program to print the first 14 numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function to print 10 times the numbers 1 - 14
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			if (j < 10)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
