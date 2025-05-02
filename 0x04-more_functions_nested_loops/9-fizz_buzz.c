2e07ab278c08979f434234bfccd794c4038919b9
root <salaonjoseph@gmail.com>
2025-05-02 05:12:31 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizzbuzz - a function to print out fizzbuzz (classic)
+*/
+void fizzbuzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
+
+/**
+ * main - a function to test the FizzBuzz function
+ * Return: Always 0
+ */
+int main(void)
+{
+	fizzbuzz();
+	return (0);
+}
