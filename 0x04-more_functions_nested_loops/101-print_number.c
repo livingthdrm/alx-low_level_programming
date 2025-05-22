c49df4f0d87935df5f730e77dacf6022b4e45b4f
livingthdrm <salaonjoseph@gmail.com>
2025-05-22 14:22:10 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_number - a function that prints an integer
  * but only using putchar
@@ -5,5 +7,31 @@
  */
 void print_number(int n)
 {
-	_putchar('0' + n);
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n > -10 && n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else if (n > -100 && n < 100)
+	{
+		_putchar('0' + n / 10);
+		_putchar('0' + n % 10);
+	}
+	else if (n > -1000 && n < 1000)
+	{
+		_putchar('0' + n / 100);
+		_putchar('0' + (n / 10) % 10);
+		_putchar('0' + n % 10);
+	}
+	else if (n > -10000 && n < 10000)
+	{
+		_putchar('0' + n / 1000);
+		_putchar('0' + (n / 100) % 10);
+		_putchar('0' + (n / 10) % 10);
+		_putchar('0' + n % 10);
+	}
 }
