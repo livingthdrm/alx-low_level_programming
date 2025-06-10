ef5bf0ab1598bb2f85454188fd7654c7f1d86953
livingthdrm <salaonjoseph@gmail.com>
2025-06-09 18:12:16 -0700

    print the sencond hald of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints halfof a string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int len = 0, n;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+
+	n = ((len - 1) / 2) + 1;
+
+	while (n <= len)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
