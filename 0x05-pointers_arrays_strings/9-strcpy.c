e7d78eb4b7cd6c7a43a0f972dc7ec175c2afb6af
livingthdrm <salaonjoseph@gmail.com>
2025-06-13 16:28:22 -0700

    a function to copy the contents of a pointer to a buffer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function to copy the content of a pointer onto a buffer
+ * @dest: the destination buffer
+ * @src: the string to copy
+ * Return: the pointer to destination buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (*(src + len) != '\n')
+	{
+		len++;
+	}
+
+	i = 0;
+
+	while (i <= len)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+	_putchar('\n');
+}
