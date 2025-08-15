a6b8838e06d1af501f99fcdb0de0b540db4720e1
livingthdrm <salaonjoseph@gmail.com>
2025-08-14 17:00:21 -0700

    a function to copy a string upto n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string upto n characters
+ * @dest: The buffer to copy to
+ * @src: the source string
+ * @n: the upto nth character to copy
+ *
+ * Return: the resultant buffer dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
