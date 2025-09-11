a97593768c54136ce7c2bb40d04b60685b07e6e3
livingthdrm <salaonjoseph@gmail.com>
2025-09-11 15:12:49 -0700

    capitalise words based on position

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,28 @@
  */
 char *cap_string(char *s)
 {
-	int len;
+	int i = 0, j;
+	char symbols[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] -= 32;
+	}
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (j < 13)
+		{
+			if (s[i] == symbols[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] -= 32;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 }
