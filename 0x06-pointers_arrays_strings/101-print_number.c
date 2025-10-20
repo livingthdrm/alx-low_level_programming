d6bf014782075853b0dd12ffd2ae040a834d3cbf
livingthdrm <salaonjoseph@gmail.com>
2025-10-20 12:40:25 -0700

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - a program to print an integer
+ * @n: the number to print
+ *
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+	unsigned int number;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -n;
+	}
+	else
+	{
+		number = n;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	while (number / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		_putchar((number / divisor) + '0');
+		number %= divisor;
+		divisor /= 10;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *s);
+void print_number(int n);
+int _putchar(char c);
 #endif
